b92e31257f1bc55d18d832d0a6ccc2230014ee4f
kronborgftp <frpa0001@stud.kea.dk>
2023-09-25 16:52:33 +0200

    Added 9 rooms. Added functionality so the user can move in between rooms. Added a limitations for where the user is able to move to. Controller usage missing.

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,3 +1,125 @@
-public class Adventure {
+import java.util.Locale;
+import java.util.Scanner;
 
+public class Adventure {
+private Rooms currentRoom;
+
+public Adventure() {
+    //Initialize rooms (names and descriptions) and connections
+    Rooms room1 = new Rooms("Room1", "ababababa");
+    Rooms room2 = new Rooms("Room2", "ababababa");
+    Rooms room3 = new Rooms("Room3", "ababababa");
+    Rooms room4 = new Rooms("Room4", "ababababa");
+    Rooms room5 = new Rooms("Room5", "ababababa");
+    Rooms room6 = new Rooms("Room6", "ababababa");
+    Rooms room7 = new Rooms("Room7", "ababababa");
+    Rooms room8 = new Rooms("Room8", "ababababa");
+    Rooms room9 = new Rooms("Room9", "ababababa");
+
+    //setting the starting room
+    currentRoom = room1;
+
+    //set directions - followed the drawing
+    //room1
+    room1.setEast(room2);
+    room1.setSouth(room4);
+
+    //room2
+    room2.setWest(room1);
+    room2.setEast(room3);
+
+    //room3
+    room3.setSouth(room6);
+    room3.setWest(room2);
+
+    //room6
+    room6.setNorth(room3);
+    room6.setSouth(room9);
+
+    //room9
+    room9.setWest(room8);
+    room9.setNorth(room6);
+
+    //room8
+    room8.setNorth(room5);
+    room8.setEast(room9);
+    room8.setWest(room7);
+
+    //room7
+    room7.setEast(room8);
+    room7.setNorth(room4);
+
+    //room4
+    room4.setSouth(room7);
+    room4.setNorth(room1);
+
+}
+
+
+//start game method
+    public void start() {
+
+    Scanner keyboard = new Scanner(System.in).useLocale(Locale.US);
+
+    //while loop so the game keeps running until the player decides to exit
+    while (true) {
+        //Display room information
+        System.out.println("You are in " + currentRoom.getName());
+        System.out.println(currentRoom.getDiscription());
+
+        //Ask user for commands
+        System.out.println("Enter a command");
+        String userInput = keyboard.nextLine().toLowerCase();
+
+        switch (userInput) {
+            case "look":
+                System.out.println(currentRoom.getDiscription());
+                break;
+            case "exit":
+                System.out.println("Goodbye");
+                System.exit(0);
+                break;
+            default:
+                //Check the users desired direction - n,s,w,e still doesn't work :(
+                if (userInput.startsWith("go ")) {
+                    String direction = userInput.substring(3);
+                    move(direction);
+                } else if (userInput.startsWith("n") || userInput.startsWith("w") || userInput.startsWith("e") || userInput.startsWith("s")) {
+                    String direction = userInput;
+                    move(direction);
+                } else {
+                    System.out.println("Invalid command");
+                }
+                break;
+        }
+    }
+
+    }
+
+    //Assign the new room
+    private void move(String direction) {
+    Rooms nextRoom = null;
+
+    switch (direction) {
+        case "north":
+            nextRoom = currentRoom.getNorth();
+            break;
+        case "east":
+            nextRoom = currentRoom.getEast();
+            break;
+        case "west":
+            nextRoom = currentRoom.getWest();
+            break;
+        case "south":
+            nextRoom = currentRoom.getSouth();
+            break;
+
+         }
+
+         if (nextRoom != null) {
+             currentRoom = nextRoom;
+         } else {
+             System.out.println("You cannot go that way.");
+         }
+    }
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,32 +1,45 @@
+import javax.management.BadAttributeValueExpException;
 import java.util.Scanner;
 
 public class UserInterface {
     private Scanner keyboard = new Scanner(System.in);
+    private Adventure adventure;
+
+    public UserInterface() {
+        adventure = new Adventure();
+    }
+
     public void startProgram() {
 
         System.out.println("Welcome to the Adventure Game!");
         System.out.println("""
             ---------------------------------------|
-            1. Type 'help' to show instructions \s |
-            2. Type 'Start' to start the game \s   |
+            1. Type 'Help' to show instructions \s  |
+            2. Type 'Start' to start the game \s    |
             3. Type 'Exit' to exit the game        |
             ---------------------------------------|
             """);
 
-        String userSelection = keyboard.nextLine();
+        //while loop so the game keeps running until the player decides to exit
+       while (true) {
+           String userSelection = keyboard.nextLine();
 
-        switch (userSelection.toLowerCase()) {
-            case "help":
-                help();
-            case "start":
-                break;
-            case "exit":
-                break;
-            default:
-                System.out.println("Choose an option from above");
-                break;
-        }
-    }
+           switch (userSelection.toLowerCase()) {
+               case "help":
+                   help();
+               case "start":
+                   adventure.start();
+                   break;
+               case "exit":
+                   System.out.println("Have a great day");
+                   System.exit(0);
+                   break;
+               default:
+                   System.out.println("Choose an option from above");
+                   break;
+           }
+       }
+       }
     private void help(){
         System.out.println("help, bla bla");
     }
