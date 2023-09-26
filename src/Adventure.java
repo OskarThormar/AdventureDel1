6124143f423f4a6faf107cc04a5cceb0f38da131
Agnethe Florentsen <agfl0001@stud.kea.dk>
2023-09-26 10:44:10 +0200

    Færdig brugerhistorie

--- src/Adventure.java
+++ src/Adventure.java
@@ -19,11 +19,19 @@ public class Adventure {
             "and you need to cross a wooden bridge to get over to the other side. Would you like to go west or south?");
     Room room4 = new Room("Room4", "You have followed the sound of water, and now you are standing on a riverbank.\n" +
             "You see a small boat floating around. Do you want to back north or continue further south?");
-    Room room5 = new Room("Room5", "");
-    Room room6 = new Room("Room6", "ababababa");
-    Room room7 = new Room("Room7", "ababababa");
-    Room room8 = new Room("Room8", "ababababa");
-    Room room9 = new Room("Room9", "ababababa");
+        Room room5 = new Room("Room5", "You have entered a great cave with a big lake covered in shimmering light.\n " +
+                "Three stones leads the way to the center of the lake, where you see a big chest with a big rusty lock on it. \n" +
+                "Would you like to go back south?");
+    Room room6 = new Room("Room6", "You are now going down a small path, there is suspeciously quit. Would you like \n"+
+            "to go further down the cave by going south, or would like to go north?");
+    //Skal vi tilføje lyd?
+    Room room7 = new Room("Room7", "You are now standing on the riverbank. Suddenly you notice a silent sound of drums. \n"+
+            "The sound of the drums get louder and louder. Would you like to go north or east?");
+    //DET STORE FARLIGE RUM!! :O :O :O :O
+    Room room8 = new Room("Room8", "You feel a sudden heat, and starts to sweat. You look down and see lava floating \n"+
+            "around you, dangerously close. Would you like to go north, west or east?");
+    Room room9 = new Room("Room9", "You are now deep into the cave, you have to climb up a steep hill to go west. \n "+
+            "Would you like to go west or north?");
 
     //setting the starting room
     currentRoom = room1;
